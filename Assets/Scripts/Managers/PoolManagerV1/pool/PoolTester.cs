e59a8049238e4a2a326cc7769871035fa86001a7
Seyyid Ahmed Doğan <seyyidahmed.dogann@gmail.com>
2016-03-08 21:47:49 +0200

    last

---
+++ Assets/Scripts/Managers/PoolManagerV1/pool/PoolTester.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoolTester : MonoBehaviour {
+
+
+    //TODO Write some elegant stuff here.
+
+    public Pool Pool;
+
+	void Start()
+    {
+        Pool = GameObject.FindGameObjectWithTag("CubePool").GetComponent<Pool>();
+
+        Pool.InstantiatePool(10);
+
+        //get from pool
+        //GameObject FromPool = Pool.GetFromPool();
+
+        //Give Random Rotation
+
+        //Give Random Transform
+
+        //Give Random Force Torque
+
+        //Add GameObject OnEnable
+        ///Pool Itself -in 5 seconds
+
+
+
+
+
+
+    }
+	
+	
+	
+	
+}
